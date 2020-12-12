6170c8eee4df66e98e2ee0088fda47d486088b7d
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2020-12-13 07:38:15 +1000

    I GIVE UP. I ADDED ;'S

--- boot/boot.js
+++ boot/boot.js
@@ -1,36 +1,36 @@
 const npmlog = require("npmlog");
-const readline = require("readline")
+const readline = require("readline");
 const dc = require("diagnostics_channel");
 
 const channel = dc.channel('hsi-njsbs-dc');
 // █▓▒░
 console.clear()
 npmlog.info("NJSBS", process.env)
-npmlog.info("NJSBS", `█████████████████████████████████████████████████████████████████`)
-npmlog.info("NJSBS", `█                                                               █▓`)
-npmlog.info("NJSBS", `█ Node.JS Battle Ships [v0.0.1 Development Unsigned Build]      █▓▒`)
-npmlog.info("NJSBS", `█                                                               █▓▒░`)
-npmlog.info("NJSBS", `█ Connect To IP Address                                         █▓▒░`)
-npmlog.info("NJSBS", `█                                                               █▓▒░`)
-npmlog.info("NJSBS", `█ Connect To IP Address [With Diagnostics Channel]              █▓▒░`)
-npmlog.info("NJSBS", `█                                                               █▓▒░`)
-npmlog.info("NJSBS", `█ Diagnostics [Diagnostics Is Not Compatible For This Game]     █▓▒░`)
-npmlog.info("NJSBS", `█                                                               █▓▒░`)
-npmlog.info("NJSBS", `█ Select an option by it's index number in the list             █▓▒░`)
-npmlog.info("NJSBS", `█                                                               █▓▒░`)
-npmlog.info("NJSBS", `█████████████████████████████████████████████████████████████████▓▒░`)
-npmlog.info("NJSBS", `▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░`)
-npmlog.info("NJSBS", ` ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░`)
-npmlog.info("NJSBS", `  ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░`)
+npmlog.info("NJSBS", `█████████████████████████████████████████████████████████████████`);
+npmlog.info("NJSBS", `█                                                               █▓`);
+npmlog.info("NJSBS", `█ Node.JS Battle Ships [v0.0.1 Development Unsigned Build]      █▓▒`);
+npmlog.info("NJSBS", `█                                                               █▓▒░`);
+npmlog.info("NJSBS", `█ Connect To IP Address                                         █▓▒░`);
+npmlog.info("NJSBS", `█                                                               █▓▒░`);
+npmlog.info("NJSBS", `█ Connect To IP Address [With Diagnostics Channel]              █▓▒░`);
+npmlog.info("NJSBS", `█                                                               █▓▒░`);
+npmlog.info("NJSBS", `█ Diagnostics [Diagnostics Is Not Compatible For This Game]     █▓▒░`);
+npmlog.info("NJSBS", `█                                                               █▓▒░`);
+npmlog.info("NJSBS", `█ Select an option by it's index number in the list             █▓▒░`);
+npmlog.info("NJSBS", `█                                                               █▓▒░`);
+npmlog.info("NJSBS", `█████████████████████████████████████████████████████████████████▓▒░`);
+npmlog.info("NJSBS", `▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░`);
+npmlog.info("NJSBS", ` ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░`);
+npmlog.info("NJSBS", `  ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░`);
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 rl.question("", (a) => {
     if (a == "1") {
         rl.question("Which IP? ", () => {
             npmlog.error("NJSBS", `Area Not Completed`)
             process.exit()
-        })
+        });
     }
-})
+});
